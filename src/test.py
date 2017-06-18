2d6a4b22ac33413b79215ef85c2d269eda956c7c
zyouyowa <transam3y@gmail.com>
2017-06-18 22:38:57 +0900

    change test script

--- src/test.py
+++ src/test.py
@@ -7,61 +7,9 @@ import chainer.functions as F
 from chainer import Chain
 from chainer import Variable, optimizers, training, iterators, cuda
 from chainer.training import extensions
+from src.main import ImagesDataset, CNN
 
 from src import myUtil
-
-
-class CNN(Chain):
-    def __init__(self):
-        super(CNN, self).__init__(
-            dconv1 = L.Convolution2D(1, 16, 3),
-            dconv2 = L.Convolution2D(16, 16, 1),
-            dconv3 = L.Convolution2D(16, 32, 3),
-            dconv4 = L.Convolution2D(32, 32, 1),
-            uconv1 = L.Deconvolution2D(32, 32, 1),
-            uconv2 = L.Deconvolution2D(32, 16, 3),
-            uconv3 = L.Deconvolution2D(16, 16, 1),
-            uconv4 = L.Deconvolution2D(16, 1, 3)
-        )
-
-    def __call__(self, X, t):
-        h = self.forward(X)
-        loss = F.mean_squared_error(h, t)
-        #chainer.report({'loss': loss}, self)
-        return loss
-
-    def forward(self, X):
-        h = F.relu(self.dconv1(X))
-        h = F.relu(self.dconv2(h))
-        h = F.relu(self.dconv3(h))
-        h = F.relu(self.dconv4(h))
-        h = F.relu(self.uconv1(h))
-        h = F.relu(self.uconv2(h))
-        h = F.relu(self.uconv3(h))
-        h = F.sigmoid(self.uconv4(h))
-        return h
-
-
-class ImagesDataset(chainer.dataset.DatasetMixin):
-    def __init__(self, dir_path='./imgs/patches', dtype=np.float32):
-        contours = myUtil.img_paths(os.path.join(dir_path, 'contour'))
-        pencils = myUtil.img_paths(os.path.join(dir_path, 'pencil'))
-        pairs = []
-        for i in range(len(contours)):
-            pairs.append((contours[i], pencils[i]))
-        self._pairs = pairs[0:len(pairs)-1]
-        self._dtype = dtype
-
-    def __len__(self):
-        return len(self._pairs)
-
-    def get_example(self, i):
-        path_c, path_p = self._pairs[i]
-        img_p = cv2.imread(path_p, cv2.IMREAD_GRAYSCALE) / 255
-        img_p = img_p.reshape(1, img_p.shape[0], img_p.shape[1]).astype(self._dtype)
-        img_c = cv2.imread(path_c, cv2.IMREAD_GRAYSCALE)/255
-        img_c = img_c.reshape(1, img_c.shape[0], img_c.shape[1]).astype(self._dtype)
-        return img_p, img_c
 
 def TestCNN():
     img = cv2.imread(os.path.abspath("./imgs/patches/contour/0.png"), cv2.IMREAD_GRAYSCALE)
